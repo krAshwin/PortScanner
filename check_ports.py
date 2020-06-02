e5ab9166e26fbaa4327dda079c20b3c4634d577e
Kumar Ashwin <krashwin00@gmail.com>
2020-06-03 02:19:50 +0530

    Adds Initial Code

--- check_ports.py
+++ check_ports.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+
+from threading import Thread
+import socket
+
+def check(port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    location = (socket.gethostname(), port)
+    res = sock.connect_ex(location)
+    print(res)
+
+    if res == 0:
+        print("Port {0} is open!".format(port))
+    else:
+        print("Port {0} is not open!".format(port))
+
+    sock.close()
+
+
+def main():
+
+    ports = (21,22,80,443)
+    for port in ports:
+        thread  = Thread(target=check, args=(port,))
+        thread.start()
+
+
+if __name__ == '__main__':
+    main()
